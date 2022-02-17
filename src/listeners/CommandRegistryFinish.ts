0c7dade40e3f68abdf23e381d3787921089675bd
bitomic <35199700+bitomic@users.noreply.github.com>
2022-02-17 13:34:42 -0600

    feat: store application commands data on command registry finish

---
+++ src/listeners/CommandRegistryFinish.ts
@@ -0,0 +1,26 @@
+import { Events, Listener } from '@sapphire/framework'
+import { ApplyOptions } from '@sapphire/decorators'
+import { env } from '../lib'
+import type { ListenerOptions } from '@sapphire/framework'
+
+@ApplyOptions<ListenerOptions>( {
+	event: Events.CommandApplicationCommandRegistryFinish,
+	once: true
+} )
+export class UserEvent extends Listener {
+	public async run(): Promise<void> {
+		const loadedCommands = env.NODE_ENV === 'development'
+			? await ( await this.container.client.guilds.fetch( env.DISCORD_DEVELOPMENT_SERVER ) ).commands.fetch()
+			: await this.container.client.application?.commands.fetch()
+		if ( !loadedCommands ) return
+		const registries = this.container.applicationCommandRegistries
+		for ( const [
+			_, command
+		] of loadedCommands ) {
+			const registry = registries.acquire( command.name )
+			if ( registry.chatInputCommands.size === 0 ) continue
+			this.container.client.chatInputCommandsData.addIdHint( command.name, command.id )
+		}
+		this.container.client.chatInputCommandsData.save()
+	}
+}
